e0c120536c13b9e334641b291008ea6047ec9330
Ben Border <ben@bunnbuns.net>
2020-09-25 14:47:18 -0400

    aaaaah

--- index.php
+++ index.php
@@ -1,22 +1,69 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="has-navbar-fixed-top">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home - Absolute Entertainment</title>
+        <link rel="icon" type="image/png" href="./assets/images/logo.png">
         <!-- Styles -->
         <link rel="stylesheet" href="./assets/css/default.css"/>
         <link rel="stylesheet" href="./assets/css/global.css"/>
         <!-- Fonts -->
         <link rel="dns-prefetch" href="https://fonts.googleapis.com">
-        <link href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Roboto&family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons&family=Roboto&family=Archivo+Black&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css"/>
     </head>
     <body>
         
-        <div class="container">
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="./">
+                <img class="logo" src="./assets/images/logo-wide-white.png" height="64">
+                </a>
 
-            <h1>Absolute Entertainment</h1>
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="mainNavbar">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="mainNavbar" class="navbar-menu">
+                <div class="navbar-start">
+                <a class="navbar-item">
+                    Home
+                </a>
+                <a class="navbar-item">
+                    Reviews
+                </a>
+                <a class="navbar-item">
+                    About
+                </a>
+
+                </div>
+
+                <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Contact Us</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="hero" style="background: #21768c url('./assets/images/hero.jpg') center center;">
+            <h1 class="title has-text-white">Absolute Entertainment.</h1>
+        </div>
+
+        <div class="container" style="margin-top: 2rem">
+
+            <!-- <h1 class="title">Absolute Entertainment</h1> -->
 
         </div>
 
